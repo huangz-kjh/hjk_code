f8480e93ac40ae316eef85ec4a8b5c2a6c173e7e
Tomj <111554512+huangz-kjh@users.noreply.github.com>
2026-04-29 18:55:36 +0800

    Create t20.py

---
+++ 100/t20.py
@@ -0,0 +1,29 @@
+class Solution(object):
+    def isValid(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        flag = 0
+        stacks = []
+        strs = {
+            ')': '(', '}': '{', ']': '[',
+            '(': '#', '{': '#', '[': '#'
+        }
+        for i in s:
+            if flag == 0:
+                stacks.append(i)
+                flag += 1
+            elif stacks[flag-1] == strs[i]:
+                stacks.pop()
+                flag -= 1
+            else:
+                stacks.append(i)
+                flag += 1
+
+        return not bool(flag)
+
+if __name__=="__main__":
+    s = input("请输入字符串")
+    sol = Solution()
+    print(sol.isValid(s))
