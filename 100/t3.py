9ab1ae18193ae400bde90aa584f983ddbdcf0101
Tomj <111554512+huangz-kjh@users.noreply.github.com>
2026-04-30 17:34:41 +0800

    Update t3.py

--- 100/t3.py
+++ 100/t3.py
@@ -1,21 +1,41 @@
+# class Solution(object):
+#     def lengthOfLongestSubstring(self, s):
+#         """
+#         :type s: str
+#         :rtype: int
+#         """
+#         sSet = {'' : 0}
+#         for i in range(len(s)):
+#             s1 = ''
+#             for j in s[i:]:
+#                 if j not in s1:
+#                     s1 += j
+#                     sSet[s1] = len(s1)
+#                 else:
+#                     sSet[s1] = len(s1)
+#                     break
+
+#         return max(sSet.values())
 class Solution(object):
     def lengthOfLongestSubstring(self, s):
         """
         :type s: str
         :rtype: int
         """
-        sSet = {'' : 0}
-        for i in range(len(s)):
-            s1 = ''
-            for j in s[i:]:
-                if j not in s1:
-                    s1 += j
-                    sSet[s1] = len(s1)
-                else:
-                    sSet[s1] = len(s1)
-                    break
+        char_set = set()
+        left = 0
+        max_len = 0
 
-        return max(sSet.values())
+        # 滑动窗口
+        for right in range(len(s)):
+            while s[right] in char_set:
+                char_set.remove(s[left])
+                left += 1
+
+            char_set.add(s[right])
+            max_len = max(max_len, right - left + 1)
+
+        return max_len
 
 
 if __name__ == "__main__":
