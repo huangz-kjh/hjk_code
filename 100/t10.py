4c4a834c5cafad7cf55c78128c45be6f747f3d1f
Tomj <111554512+huangz-kjh@users.noreply.github.com>
2026-05-11 15:47:59 +0800

    Update t10.py

--- 100/t10.py
+++ 100/t10.py
@@ -1,37 +1,37 @@
 class Solution(object):
     def isMatch(self, s, p):
+        """
+        :type s: str
+        :type p: str
+        :rtype: bool
+        """
         m, n = len(s), len(p)
-
-        # dp[i][j]: s[:i] 和 p[:j] 是否匹配
         dp = [[False] * (n + 1) for _ in range(m + 1)]
 
         dp[0][0] = True  # 空串匹配空串
 
-        # 处理 s="" 的情况
+        # s = ''的情况
         for j in range(2, n + 1):
             if p[j - 1] == '*':
                 dp[0][j] = dp[0][j - 2]
 
-        for i in range(1, m + 1):
-            for j in range(1, n + 1):
-
-                # 普通字符 or '.'
+        # s != ''的情况
+        i = 1
+        for j in range(1, n + 1):
+            for i in range(1,  m + 1):
                 if p[j - 1] == s[i - 1] or p[j - 1] == '.':
                     dp[i][j] = dp[i - 1][j - 1]
-
-                # '*' 情况
+                    i += 1
                 elif p[j - 1] == '*':
                     # 匹配 0 次
-                    dp[i][j] = dp[i][j - 2]  # 删除 “前一个字符 + *”
+                    dp[i][j] = dp[i][j - 2]
 
-                    # 匹配 >=1 次
-                    if p[j - 2] == s[i - 1] or p[j - 2] == '.':
-                        dp[i][j] |= dp[i - 1][j]
-                        # dp[i][j] = dp[i][j] or dp[i - 1][j]
+                    # 匹配 大于 1 次
+                    if s[i - 1] == p[j - 2] or p[j - 2] == '.':
+                        dp[i][j] = dp[i][j] or dp[i - 1][j]
+                        i += 1
 
         return dp[m][n]
-
-
 
 
 if __name__ == "__main__":
